dc1dc10674472213bdf21ad66ac88a756b0aba47
Somaya <somaya.bounouar@gmail.com>
2017-03-07 09:40:59 -0800

    Specify Error instead of Exception

--- exercism/hamming/hamming.js
+++ exercism/hamming/hamming.js
@@ -4,7 +4,7 @@ Hamming.prototype.compute = function (string1, string2) {
   var counter = 0
 
   if(string1.length !== string2.length) {
-    throw "DNA strands must be of equal length."
+    throw new Error("DNA strands must be of equal length.")
   }
 
   for(var i = 0; i < string1.length; i++) {
