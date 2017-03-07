070b19ef17a3805832531ec302e1a243100db803
thelady <dihaynes77@gmail.com>
2017-03-06 17:58:33 -0800

    moved counter in script

--- exercism/hamming/hamming.js
+++ exercism/hamming/hamming.js
@@ -1,11 +1,11 @@
 var Hamming = function() {}
 
 Hamming.prototype.compute = function (string1, string2) {
+  var counter = 0
+
   if(string1.length !== string2.length) {
     throw "DNA strands must be of equal length."
   }
-
-  var counter = 0
 
   for(var i = 0; i < string1.length; i++) {
     if(string1[i] !== string2[i]) {
