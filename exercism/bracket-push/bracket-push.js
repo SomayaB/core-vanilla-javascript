d8c63db1e2d02d8b64a89ef5f3eda43564a2a04c
thelady <dihaynes77@gmail.com>
2017-03-08 17:43:06 -0800

    add bracket-push file and spec

--- exercism/bracket-push/bracket-push.js
+++ exercism/bracket-push/bracket-push.js
@@ -1,41 +1,63 @@
-function bracket(string) {
-  // if (string === '{}') {
-  //   return true
-  // }
-  // else {
-  //   return false
-  // }
-  var openingBracket = '['
-  var closingBracket = ']'
-  var openingCurlyBrace = '{'
-  var closingCurlyBrace = '}'
+function bracket(str){
+var paren = ['(', ')']
+var curly = ['{','}']
+var square = ['[',']']
+var closings = [')', '}', ']']
 
-  if (string.includes(openingBracket) && string.includes(closingBracket) ) {
-      return true
-  } else {
-    return false
-  }
- if (string.includes(openingCurlyBrace) && string.includes(closingCurlyBrace) ) {
-      return true
-  } else {
-    return false
-  }
-//if string.includes(openingBracket) then string must include closingBracket && if string includes openingCurlyBrace then string must include closingCurlyBrace, return true. Else return false.
+var arr = str.split('')
+var stack = []
 
+  for( var i in arr ){
+    if( closings.includes( arr[i] ) ){
+    var type = check( arr[i] )
+      if( check(stack.pop()) !== type ){
+        return false
+      }
+    }else{
+      stack.push( arr[i] )
+    }
+  }
+
+  function check(element){
+    if( paren.includes(element) ){
+      return 'paren'
+    }else if( curly.includes(element) ){
+      return 'curly'
+    }else if( square.includes(element) ){
+      return 'square'
+    }else{
+      return false
+    }
+  }
+  if( stack.length > 0 ) return false
+  return true
 }
 
-// var bracket = function(element) {
-//   if(element == '{}')
-//     return true
-//       // if(element == '}')
-//       //   return
-//       //     if(element == '[')
-//       //       return
-//       //         if(element == '[')
-//       //           return
 
 
-// }
+// var bracket_pairs = {
+//   "{": "}",
+//   "[":"]",
+//   "(":")"
+// };
+
+// function bracket(brackets) {
+//   /* checks that bracket syntax is correct */
+//   var stack = [];
+//   for( var i = 0; i < brackets.length; i++) {
+//     var character = brackets[i];
+//     if( bracket_pairs.hasOwnProperty(character) ) {
+//       stack.push(bracket_pairs[character]);
+//       console.log(bracket_pairs[character]);
+//     } else if( stack.length !== 0 && stack[stack.length - 1] === character) {
+//       stack.pop();
+//     } else {
+//       return false;
+//     }
+//   }
+//   return stack.length === 0;
+// };
+
 
 
 
